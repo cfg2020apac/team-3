96f96c477fdf2f89aa5509ac64993867e358c3b0
kev-inc <kevinchanxy@gmail.com>
2020-10-10 00:59:14 +0800

    added myevents and recommendedevents to events tab

--- ionic/BWSApp/src/components/EventsCard.tsx
+++ ionic/BWSApp/src/components/EventsCard.tsx
@@ -1,28 +1,49 @@
-import React from 'react'
-import { IonItem, IonCard, IonCardHeader, IonCardSubtitle, IonCardTitle, IonCardContent } from '@ionic/react';
-import moment from 'moment'
+import React from "react";
+import {
+  IonItem,
+  IonCard,
+  IonCardHeader,
+  IonCardSubtitle,
+  IonCardTitle,
+  IonCardContent,
+  IonBadge,
+} from "@ionic/react";
+import moment from "moment";
 interface ComponentProps {
-  id: string,
-  title: string,
-  imgSrc: string,
-  description: string,
-  start_datetime: string
+  id: string;
+  title: string;
+  imgSrc: string;
+  description: string;
+  start_datetime: string;
+  tags: { name: string }[];
 }
 
-const EventsCard: React.FC<ComponentProps> = ({id, title, imgSrc, description, start_datetime}) => {
-  return(
-    <IonItem>
+const EventsCard: React.FC<ComponentProps> = ({
+  id,
+  title,
+  imgSrc,
+  description,
+  start_datetime,
+  tags,
+}) => {
+  return (
+    <IonItem lines="none">
       <IonCard routerLink={`/tabs/event/` + id}>
-        <img src={imgSrc} alt=""/>
+        <img src={imgSrc} alt="" />
         <IonCardHeader>
-          <IonCardSubtitle>{moment(start_datetime).format("MMMM Do YYYY")}</IonCardSubtitle>
+          <IonCardSubtitle>
+            {moment(start_datetime).format("MMMM Do YYYY")}
+          </IonCardSubtitle>
           <IonCardTitle>{title}</IonCardTitle>
         </IonCardHeader>
         <IonCardContent>
-          {description}
+          {tags.map((tag, index) => (
+            <IonBadge key={index} style={{marginRight: '8px'}}>{tag.name}</IonBadge>
+          ))}
+          <p>{description}</p>
         </IonCardContent>
       </IonCard>
     </IonItem>
-  )
-}
-export default EventsCard
+  );
+};
+export default EventsCard;
